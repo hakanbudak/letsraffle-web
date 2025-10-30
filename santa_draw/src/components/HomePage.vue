6969dc78efade1f967ee46727517e804451822ae
Hakan Budak <hakanbudk0@gmail.com>
2025-10-30 22:24:22 +0300

    chore(frontend): remove deprecated HomePage component after moving to views (#1)

--- santa_draw/src/components/HomePage.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="min-h-screen flex flex-col items-center justify-center text-center">
-    <h1 class="text-3xl font-bold text-green-600">
-      🎅 Welcome to Santa's Draw
-    </h1>
-  </div>
-</template>
